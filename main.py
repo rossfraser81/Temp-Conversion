660b0e53cd526d789175c609a3d6bfb673e5bf2e
rossfraser81 <85490158+rossfraser81@users.noreply.github.com>
2021-08-18 12:59:29 +0000

    corrected comments

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 print("Welcome to the Temperature Converter Program")
 
-#user input to get miles per hour value
+#user input to get fahrenheit value
 fahr = float(input("\nEnter the the temperature in degrees Fahrenheit you wish to convert: "))
 
 #convert to celsius and kelvin
-cel = (fahr-32)*0.5556
-kel = cel+273.15
+cel = (fahr - 32) * 0.5556
+kel = cel + 273.15
 
 #round to 4 dp
-fahr = round(fahr,4)
-cel = round(cel,4)
-kel = round(kel,4)
+fahr = round(fahr, 4)
+cel = round(cel, 4)
+kel = round(kel, 4)
 
 print("\nDegrees Fahrenheit:\t" + str(fahr))
 print("Degrees Celsius:\t" + str(cel))
